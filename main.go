03b6f34a9df78abcac7ea5b68e8c5e35b96cfc61
ThibaultF <thibault.frescaline@ynov.com>
2024-04-19 13:43:26 +0200

    forum start

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// constant for the redirect
+const port = ":8080"
+
+// home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "tmpl/home.html")
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "tmpl/login.html")
+}
+
+// function that creat a table User
+func createTableUser(db *sql.DB) {
+	//creating the user table if not already created
+	_, err := db.Exec(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+			isAdmin BOOL NOT NULL,
+			picture BLOB
+        )
+    `)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func main() {
+	//open the database with sqlite3
+	db, err := sql.Open("sqlite3", "database.db")
+	if err != nil {
+		panic(err.Error())
+	}
+	//creat the 2 tables
+	createTableUser(db)
+	defer db.Close()
+
+	http.HandleFunc("/home", Home)
+	http.HandleFunc("/login", Login)
+
+	//load the CSS and the images
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
+
+	//start the local host
+	fmt.Println("\n(http://localhost:8080/home) - Server started on port", port)
+	http.ListenAndServe(port, nil)
+}
