267bf3664551fdc73d9c93097b58045eebf0de43
ThibaultF <thibault.frescaline@ynov.com>
2024-05-14 15:42:01 +0200

    added reading db posts

--- functions/handler.go
+++ functions/handler.go
@@ -9,27 +9,38 @@ import (
 )
 
 type UserData struct {
-	IsLoggedIn bool
+	IsLoggedIn     bool
 	ProfilePicture string
-	Categories []string
+	Categories     []string
+	Posts		  []PostData
+}
+
+type PostData struct {
+	Id       int
+	Title    string
+	Content  string
+	Category string
+	Author   string
 }
 
 // home page
 func Home(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	token := GetSessionToken(r)
-
 	fmt.Println("token: ", token)
 
+	posts := getPosts(w, db)
+	fmt.Println("posts: ", posts)
+
 	//get the user data from the database
-    row := db.QueryRow("SELECT isAdmin, isBanned, pp FROM users WHERE UUID=?", token)
-    var isAdmin, isBanned bool
+	row := db.QueryRow("SELECT isAdmin, isBanned, pp FROM users WHERE UUID=?", token)
+	var isAdmin, isBanned bool
 	var pp []byte
 
 	//scan and get the data
 	err := row.Scan(&isAdmin, &isBanned, &pp)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			serveHomePage(w, false, "", nil)
+			serveHomePage(w, false, "", nil, posts)
 			return
 		} else {
 			http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
@@ -54,17 +65,50 @@ func Home(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		categories = append(categories, category)
 	}
 
-	serveHomePage(w, true, profilePicture, categories)
+	serveHomePage(w, true, profilePicture, categories, posts)
 }
 
-func serveHomePage(w http.ResponseWriter, isLoggedIn bool, pp string, categories []string) {
-    userData := UserData{IsLoggedIn: isLoggedIn, ProfilePicture: pp, Categories: categories}
-    tmpl, err := template.ParseFiles("tmpl/home.html")
-    if err != nil {
-        http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
-        return
-    }
-    if err := tmpl.Execute(w, userData); err != nil {
-        http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
-    }
+func getPosts(w http.ResponseWriter, db *sql.DB) []PostData {
+	var posts []PostData
+	rows, err := db.Query(`SELECT id, title, content, date, category, author FROM posts ORDER BY date DESC`)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+		return nil
+	}
+	for rows.Next() {
+		var id, category, author int
+		var title, content, date string
+		err := rows.Scan(&id, &title, &content, &date, &category, &author)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+			return nil
+		}
+		var categoryStr, authorStr string
+		row := db.QueryRow("SELECT name FROM categories WHERE id=?", category)
+		err = row.Scan(&categoryStr)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+			return nil
+		}
+		row = db.QueryRow("SELECT username FROM users WHERE id=?", author)
+		err = row.Scan(&authorStr)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+			return nil
+		}
+		posts = append(posts, PostData{Id: id, Title: title, Content: content, Category: categoryStr, Author: authorStr})
+	}
+	return posts
+}
+
+func serveHomePage(w http.ResponseWriter, isLoggedIn bool, pp string, categories []string, posts []PostData) {
+	userData := UserData{IsLoggedIn: isLoggedIn, ProfilePicture: pp, Categories: categories, Posts: posts}
+	tmpl, err := template.ParseFiles("tmpl/home.html")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+		return
+	}
+	if err := tmpl.Execute(w, userData); err != nil {
+		http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+	}
 }
